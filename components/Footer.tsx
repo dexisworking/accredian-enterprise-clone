e9bc95850bec1e7e43b64e148f03dc723dc80a36
dexisworking <dibyanshu.singh2004@gmail.com>
2026-04-23 20:06:31 +0530

    feat: create Footer component with navigation, social links, and contact information

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 export function Footer({ onEnquire }: { onEnquire?: () => void }) {
   return (
     <footer className="mt-12 bg-[#1d1d1d] sm:bg-white sm:mt-24 py-10" data-cur="cursor">
-      <div className="max-w-[85rem] w-full px-[22px] sm:px-0 mx-auto" data-cur="cursor">
+      <div className="w-full px-[22px] sm:px-12" data-cur="cursor">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center border-b border-gray-700 pb-6" data-cur="cursor">
-          <div>
+          <div data-cur="cursor">
             <img 
               className="hidden sm:block" 
               src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/logo.webp" 
@@ -16,15 +16,15 @@ export function Footer({ onEnquire }: { onEnquire?: () => void }) {
               alt="Accredian Mobile" 
               style={{ width: "9rem" }} 
             />
-            <div className="flex gap-4 mt-4">
+            <div className="flex gap-4 mt-4" data-cur="cursor">
               <a href="https://facebook.com/accredianlearn" className="sm:text-gray-700 text-white hover:text-[#1A73E8]">
-                <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FacebookIcon">
-                  <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z"></path>
+                <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FacebookIcon" data-cur="pointer">
+                  <path d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2m13 2h-2.5A3.5 3.5 0 0 0 12 8.5V11h-2v3h2v7h3v-7h3v-3h-3V9a1 1 0 0 1 1-1h2V5z" data-cur="pointer"></path>
                 </svg>
               </a>
               <a href="https://www.linkedin.com/company/accredianedu/" className="sm:text-gray-700 text-white hover:text-[#1A73E8]">
-                <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LinkedInIcon">
-                  <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1-2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"></path>
+                <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="LinkedInIcon" data-cur="pointer">
+                  <path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1-2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z" data-cur="pointer"></path>
                 </svg>
               </a>
               <a href="https://twitter.com/accredianedu" className="sm:text-gray-700 text-white hover:text-[#1A73E8]">
@@ -34,7 +34,7 @@ export function Footer({ onEnquire }: { onEnquire?: () => void }) {
               </a>
               <a href="https://www.instagram.com/accredian_edu" className="sm:text-gray-700 text-white hover:text-[#1A73E8]">
                 <svg className="w-5 h-5" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="InstagramIcon">
-                  <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5 5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
+                  <path d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path>
                 </svg>
               </a>
               <a href="https://www.youtube.com/channel/UCE0L_4ADPU2iyKnDJ0xRzyA" className="sm:text-gray-700 text-white hover:text-[#1A73E8]">
@@ -59,7 +59,7 @@ export function Footer({ onEnquire }: { onEnquire?: () => void }) {
           <div data-cur="cursor">
             <h3 className="font-normal sm:font-semibold text-lg text-white sm:text-black" data-cur="cursor">Accredian</h3>
             <ul className="mt-2 text-white sm:text-gray-700 space-y-2" data-cur="cursor">
-              <li data-cur="cursor"><a href="https://accredian.com/About" className="hover:text-[#1A73E8]">About</a></li>
+              <li data-cur="cursor"><a href="https://accredian.com/About" className="hover:text-[#1A73E8]" data-cur="pointer">About</a></li>
               <li data-cur="cursor"><a href="https://blog.accredian.com/" className="hover:text-[#1A73E8]">Blog</a></li>
               <li data-cur="cursor"><a href="https://accredian.com/whyaccredian" className="hover:text-[#1A73E8]">Why Accredian</a></li>
             </ul>
