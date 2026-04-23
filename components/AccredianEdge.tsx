712dc1acf5a08192c7e39fe81308f63829c71e25
dexisworking <dibyanshu.singh2004@gmail.com>
2026-04-23 19:38:28 +0530

    feat: create AccredianEdge component to display strategic training and course offerings

--- components/AccredianEdge.tsx
+++ components/AccredianEdge.tsx
@@ -145,6 +145,49 @@ export function AccredianEdge() {
         </div>
       </section>
 
+      {/* Who Should Join Section */}
+      <div className="mt-12 sm:mt-28 xl:px-6 px-4 lg:mx-10 bg-[#1A73E8] sm:rounded-lg flex flex-col md:flex-row" data-cur="cursor">
+        <div className="md:w-1/2 pt-12 md:pl-6 text-white flex flex-col justify-between" data-cur="cursor">
+          <div>
+            <h4 className="text-lg sm:text-xl font-medium" data-cur="cursor">Who Should Join?</h4>
+            <h1 className="text-2xl md:text-[40px] leading-[39px] capitalize mt-2 font-semibold" data-cur="cursor">Strategic Skill Enhancement</h1>
+          </div>
+          <div className="w-[300px] justify-center hidden md:block">
+            <img src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/imagehuman.png" alt="Human Illustration" data-cur="cursor" />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 py-12 text-white gap-10" data-cur="cursor">
+          <div className="flex gap-4 sm:gap-2 flex-row sm:flex-col" data-cur="cursor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-monitor-check w-10 h-10 sm:w-14 sm:h-14 mb-2"><path d="m9 10 2 2 4-4"></path><rect width="20" height="14" x="2" y="3" rx="2"></rect><path d="M12 17v4"></path><path d="M8 21h8"></path></svg>
+            <div>
+              <h2 className="text-[16px] sm:text-[22px] font-semibold">Tech Professionals</h2>
+              <p className="text-sm sm:text-md text-gray-200">Enhance expertise, embrace tech, drive innovation.</p>
+            </div>
+          </div>
+          <div className="flex gap-4 sm:gap-2 flex-row sm:flex-col">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-monitor-x w-10 h-10 sm:w-14 sm:h-14 mb-2"><path d="m14.5 12.5-5-5"></path><path d="m9.5 12.5 5-5"></path><rect width="20" height="14" x="2" y="3" rx="2"></rect><path d="M12 17v4"></path><path d="M8 21h8"></path></svg>
+            <div>
+              <h2 className="text-[16px] sm:text-[22px] font-semibold" data-cur="cursor">Non-Tech Professionals</h2>
+              <p className="text-sm sm:text-md text-gray-200" data-cur="cursor">Adapt digitally, collaborate in tech environments.</p>
+            </div>
+          </div>
+          <div className="flex gap-4 sm:gap-2 flex-row sm:flex-col" data-cur="cursor">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-graduation-cap w-10 h-10 sm:w-14 sm:h-14 mb-2" data-cur="cursor"><path d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"></path><path d="M22 10v6"></path><path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"></path></svg>
+            <div>
+              <h2 className="text-[16px] sm:text-[22px] font-semibold">Emerging Professionals</h2>
+              <p className="text-sm sm:text-md text-gray-200" data-cur="cursor">Develop powerful skills for rapid career growth.</p>
+            </div>
+          </div>
+          <div className="flex gap-4 sm:gap-2 flex-row sm:flex-col">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-briefcase w-10 h-10 sm:w-14 sm:h-14 mb-2"><path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path><rect width="20" height="14" x="2" y="6" rx="2"></rect></svg>
+            <div>
+              <h2 className="text-[16px] sm:text-[22px] font-semibold">Senior Professionals</h2>
+              <p className="text-sm sm:text-md text-gray-200">Strengthen leadership, enhance strategic decisions.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Mobile View SVG */}
       <div className="flex sm:hidden w-full justify-center items-center mb-8">
         <img 
