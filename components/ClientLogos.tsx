34d0fc174a7c75549fc37afb6e10542d6b970846
dexisworking <dibyanshu.singh2004@gmail.com>
2026-04-23 19:11:05 +0530

    feat: add ClientLogos component with grid and marquee layout

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -7,24 +7,64 @@ export function ClientLogos() {
   return (
     <section id="clients" className="py-20 bg-slate-50 relative overflow-hidden">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="text-center mb-16">
-          <h2 className="text-[28px] sm:text-[32px] font-extrabold text-[#000000]">
-            Trusted by Top <span className="text-[#1A73E8]">Enterprises</span> Worldwide
+        <div className="text-center mx-2 mb-16" data-cur="cursor">
+          <h2 className="text-2xl sm:text-4xl font-bold text-gray-900 leading-tight" data-cur="cursor">
+            Our Proven <span className="text-[#1A73E8]" data-cur="cursor">Partnerships</span>
           </h2>
+          <p className="text-sm sm:text-lg text-gray-700 mt-3 m-auto" data-cur="cursor">
+            Successful Collaborations With the <span className="text-[#1A73E8]" data-cur="cursor">Industry’s Best</span>
+          </p>
         </div>
         
         {/* Desktop Grid */}
-        <div className="hidden md:grid grid-cols-6 gap-12 items-center opacity-60">
-          {clientLogos.map((client) => (
-            <div key={client.name} className="flex justify-center grayscale hover:grayscale-0 transition-all duration-300">
-              <img 
-                src={client.logo} 
-                alt={client.name}
-                className="h-10 w-auto object-contain max-w-[140px]"
-              />
-            </div>
-          ))}
-        </div>
+        <ul className="hidden sm:grid grid-cols-3 md:grid-cols-6 xl:grid-cols-6 gap-6 md:gap-8 xl:gap-12" data-cur="cursor">
+          <li className="flex justify-center items-center sm:p-4" data-cur="cursor">
+            <img 
+              src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/rel.png" 
+              alt="client" 
+              className="object-contain w-14 h-14 sm:w-24 sm:h-24 md:w-24 md:h-24" 
+              data-cur="cursor" 
+            />
+          </li>
+          <li className="flex justify-center items-center sm:p-4" data-cur="cursor">
+            <img 
+              src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/hcl.png" 
+              alt="client" 
+              className="object-contain w-16 h-16 sm:w-24 sm:h-24 md:w-40 md:h-40" 
+              data-cur="cursor" 
+            />
+          </li>
+          <li className="flex justify-center items-center sm:p-4" data-cur="cursor">
+            <img 
+              src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/ibm.png" 
+              alt="client" 
+              className="object-contain w-14 h-14 sm:w-24 sm:h-24 md:w-24 md:h-24" 
+              data-cur="cursor" 
+            />
+          </li>
+          <li className="flex justify-center items-center sm:p-4" data-cur="cursor">
+            <img 
+              src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/crif.png" 
+              alt="client" 
+              className="object-contain w-14 h-14 sm:w-24 sm:h-24 md:w-24 md:h-24" 
+              data-cur="cursor" 
+            />
+          </li>
+          <li className="flex justify-center items-center sm:p-4" data-cur="cursor">
+            <img 
+              src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/adp.svg" 
+              alt="client" 
+              className="object-contain w-14 h-14 sm:w-24 sm:h-24 md:w-24 md:h-24" 
+            />
+          </li>
+          <li className="flex justify-center items-center sm:p-4" data-cur="cursor">
+            <img 
+              src="https://storage.googleapis.com/accredian-assets/Frontend_Assests/Images/Accredian-react-site-images/other/bayer.svg" 
+              alt="client" 
+              className="object-contain w-12 h-12 sm:w-20 sm:h-20 md:w-24 md:h-24" 
+            />
+          </li>
+        </ul>
 
         {/* Mobile Marquee */}
         <div className="md:hidden relative flex overflow-x-hidden">
